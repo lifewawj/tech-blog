afcfc5dba9c6ee87ade4ac638a1c5c26d4dfd9a3
lifewawj <arevaloaj01@gmail.com>
2024-02-02 17:28:31 -0800

    Sync our database to our SQL database with Sequalize

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+
+    : new Sequelize(
+        // Access the .env SECRET file and grabs the variable named DB_NAME
+        process.env.DB_NAME,
+        // Access the .env SECRET file and grabs the variable named DB_USER
+        process.env.DB_USER,
+        // Access the .env SECRET file and grabs the variable named DB_PASSWORD
+        process.env.DB_PASSWORD, {
+        // Database location
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+// Exports the sequalize const var, making it available to use in other files
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+// NPM Packages Included
+const path = require('path');
+const express = require('express'); 
+const sequelize = require('./config/connection');
 
 // Create a const var for express(), easier to use
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Create a PORT const var to specify where the server should run
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log('Now listening'));
+// Syncs our SQL db to our server, and inits our server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
