66ae8f969114feabd51ac62de02afaa630d4f8fc
lifewawj <arevaloaj01@gmail.com>
2024-02-03 00:23:40 -0800

    Server is up and running with installing express-handlebars and its basic route with text on page saying Hello World

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 // NPM Packages Included
 const path = require('path');
-const express = require('express'); 
-const sequelize = require('./config/connection');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-// Create a const var for express(), easier to use
-const app = express();
+const sequelize = require('./config/connection'); // Sequalized Connection with mySQL Database
+
+// Creating const var(s)
+const app = express(); // Create a const var for express(), easier to use
+const PORT = process.env.PORT || 3001; // Create a PORT const var to specify where the server should run
+const hbs = exphbs.create({}); // Create a hbs const var
+
+// The following two lines of code are setting Handlebars.js as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Create a PORT const var to specify where the server should run
-const PORT = process.env.PORT || 3001;
+// Server Routes
+const routes = require('./controllers/homeRoutes'); // Create a routes const var to GRAB all the exported router from controllers
+app.use(routes);
 
 // Syncs our SQL db to our server, and inits our server
 sequelize.sync({ force: false }).then(() => {
