67c351c2317b92e835cb90accd2a85a7bc788852
lifewawj <arevaloaj01@gmail.com>
2024-02-05 18:13:06 -0800

    Made Nav Category Buttons interactable making them send the user the correct route with the correct btn, ON CLICK

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,39 @@
 const router = require('express').Router();
 
+
+// Homepage Route
 router.get('/', (req, res) => {
     try {
-        res.render('homepage', { title: 'Tech Blog | Homepage'})
+        res.render('home', { title: 'Tech Blog | Home'})
     } catch (err) {
         res.status(500).json(err);
     };
 });
 
+// Dashboard Route
+router.get('/dashboard', (req, res) => {
+    try {
+        res.render('dashboard',{ title: 'Tech Blog | Dashboard'})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+
+// Login Route
 router.get('/login', (req, res) => {
     try {
-        res.render('login', { title: 'Tech Blog | Login page'})
+        res.render('login', { title: 'Tech Blog | Login'})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Logout Route
+router.get('/logout', (req, res) => {
+    try {
+        res.render('logout',{ title: 'Tech Blog | Logout'})
     } catch (err) {
         res.status(500).json(err);
     }
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
-const homeBtn = document.getElementById('homeBtn');
-const dashboardBtn = document.getElementById('dashboardBtn');
-const loginBtn = document.getElementById('loginBtn');
-const logoutBtn = document.getElementById('logoutBtn');
+const homeBtn = document.getElementById('home');
+const dashboardBtn = document.getElementById('dashboard');
+const loginBtn = document.getElementById('login');
+const logoutBtn = document.getElementById('logout');
+
+homeBtn.addEventListener('click', () => {
+    window.location.href = "/"
+});
+
+dashboardBtn.addEventListener('click', () => {
+    window.location.href = "/dashboard"
+});
+
+loginBtn.addEventListener('click', () => {
+    console.log("Login Button has been clicked!")
+    window.location.href = "/login"
+});
+
+logoutBtn.addEventListener('click', () => {
+    window.location.href = "/logout"
+});
+
