7f84352f387baf471a9f8b85cdc7d21621e3493f
lifewawj <arevaloaj01@gmail.com>
2024-02-07 02:55:49 -0800

    Created a User Model

---
+++ model/user-model.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class User extends Model { }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+
+    {
+        // Link to database connection
+        sequelize,
+        // Set to false to remove `created_at` and `updated_at` fields
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        // Assigning the name for our table 'category'
+        modelName: 'user',
+    }
+)
